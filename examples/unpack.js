dc5fc52a0dfaff1f10b4d0f0123d355279e3f462
Thomas Hobson <git@hexf.me>
2019-10-11 21:29:14 +1300

    Jet patching example

--- examples/unpack.js
+++ examples/unpack.js
@@ -1,9 +1,11 @@
 const datajet = require('../src/main/node');
 const path = require('path');
 
+const patch = require('./examplepatch.json');
+
 var up = datajet.packer.unpack(
 	path.resolve(__dirname, '../test/jets/steamwindows_6.4.0.jet'),
 	datajet.version.steam.windows['6.4.0']
 );
-
-up.saveFiles(path.resolve(__dirname, './tmp'));
+up.addPatch(patch);
+up.saveFiles(path.resolve(__dirname, 'patches'));
