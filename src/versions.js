ea9f87e710c5220560c599ec52bc312c522c7602
Thomas Hobson <git@hexf.me>
2019-10-12 17:35:15 +1300

    New versioning system, split into a different file

--- src/versions.js
+++ src/versions.js
@@ -1,113 +1,49 @@
+var jets = require('./jets.json');
+
+function convertFormat(input, platformFriendlyName){
+	var versions = Object.keys(input);
+	var res = {};
+	versions.forEach((version) => {
+		var e = input[version];
+		var o = {
+			password: e.password,
+			hash: e.hashes,
+			version: version,
+			platform: platformFriendlyName
+		};
+		res[version] = o;
+	});
+	return res;
+}
+
 var versions = {
+	winrt: convertFormat(jets.winrt, 'Windows RT'),
 	steam: {
-		windows: {
-			'6.4.0': {
-				platform: 'Steam Windows',
-				version: '6.4.0',
-				password: '6DAD88C792DD3E6C',
-				hash: [
-					{
-						type: 'md5',
-						hash: 'c78cacc311d603cdd24111ea529d98e5'
-					}
-				]
-			},
-			'6.3.2': {
-				platform: 'Steam Windows',
-				version: '6.3.2',
-				password: '9E7C0AFC9BCD1B0A',
-				hash: [
-					{
-						type: 'md5',
-						hash: 'yeahidontknowlol'
-					}
-				]
-			}
-		},
-		mac: {
-			'6.4.0': {
-				platform: 'Steam MacOS',
-				version: '6.4.0',
-				password: '1058BF9702322AD9',
-				hash: [
-					{
-						type: 'md5',
-						hash: 'yeahidontknowlol'
-					}
-				]
-			},
-			'6.3.2': {
-				platform: 'Steam MacOS',
-				version: '6.3.2',
-				password: '8844B52CFB1B362E',
-				hash: [
-					{
-						type: 'md5',
-						hash: 'yeahidontknowlol'
-					}
-				]
-			}
-		}
+		windows: convertFormat(jets.steam.windows, 'Steam Windows'),
+		macos: convertFormat(jets.steam.macos, 'Steam MacOS')
 	},
-	android: {
-		'6.4.0': {
-			platform: 'Android',
-			version: '6.4.0',
-			password: '400AFE813C341CEA',
-			hash: [
-				{
-					type: 'md5',
-					hash: 'yeahidontknowlol'
-				}
-			]
-		},
-		'6.3.2': {
-			platform: 'Android',
-			version: '6.3.2',
-			password: '6597296902FFCC03',
-			hash: [
-				{
-					type: 'md5',
-					hash: 'yeahidontknowlol'
-				}
-			]
-		}
-	},
-	ios: {
-		'6.4.0': {
-			platform: 'iOS',
-			version: '6.4.0',
-			password: '2E1BE7CCF3049725',
-			hash: [
-				{
-					type: 'md5',
-					hash: 'yeahidontknowlol'
-				}
-			]
-		},
-		'6.3.2': {
-			platform: 'iOS',
-			version: '6.3.2',
-			password: 'C3A2874DE8867F27',
-			hash: [
-				{
-					type: 'md5',
-					hash: 'yeahidontknowlol'
-				}
-			]
-		}
-	}
+	ios: convertFormat(jets.ios, 'iOS'),
+	android: convertFormat(jets.android, 'Android')
 };
+
+var platforms = [
+	versions.steam.windows,
+	versions.steam.mac,
+	versions.android,
+	versions.ios
+];
+
+var allVersions = [];
+platforms.forEach((platform) => {
+	if (platform == undefined) return;
+	var versions = Object.keys(platform);
+	versions.forEach((version) => {
+		allVersions.push(platform[version]);
+	});
+});
+
 var metadata = {
-	platforms: [
-		versions.steam.windows,
-		versions.steam.mac,
-		versions.android,
-		versions.ios
-	],
-	versions: [
-		'6.4.0',
-		'6.3.2'
-	]
+	platforms: platforms,
+	allVersions: allVersions
 };
 module.exports = Object.assign(versions, metadata);
